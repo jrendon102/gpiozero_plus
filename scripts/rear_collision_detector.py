91c08f5bf22712e1949e9124f98d0f8ee441f3d5
Julian Rendon <91917978+jrendon102@users.noreply.github.com>
2023-04-07 17:54:46 -0400

    dev/joystick-teleop (#9)
    Overview
    Implement new logic for teleoperation via joystick.
    
    Major Changes
    - Included logic so that joystick teleop is possible.
    - Adjusted rear collision to handle new logic for joystick velocity messages.
    
    Minor Changes
    - Modified rosparams for expansion board.

--- scripts/rear_collision_detector.py
+++ scripts/rear_collision_detector.py
@@ -3,6 +3,7 @@
 ROS wrapper node that communicates with the Ultrasonic Sensor interface on the Yahboom G1 Tank.
 """
 import sys
+from threading import Lock
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -31,10 +32,10 @@ class ReverseSafetySensor:
             # Set up Ultrasonic sensor
             rospy.loginfo("Setting up ultrasonic sensor.")
             self.collision_sensor = YBUltrasonic(
-                echo_pin=rospy.get_param("/hardware/ultrasonic/echo"),
-                trig_pin=rospy.get_param("/hardware/ultrasonic/trig"),
+                echo_pin=rospy.get_param("/exp_board/ultrasonic/echo"),
+                trig_pin=rospy.get_param("/exp_board/ultrasonic/trig"),
             )
-            self.collision_threshold = rospy.get_param("/hardware/ultrasonic/min_dist")
+            self.collision_threshold = rospy.get_param("/exp_board/ultrasonic/min_dist")
         except KeyError:
             rospy.logerr(f"{ERR_PARAM}")
             sys.exit()
@@ -47,7 +48,31 @@ class ReverseSafetySensor:
         )
 
         # Subscriber
-        self.velocity_sub = rospy.Subscriber("/cmd_vel", Twist, self.velocity_callback)
+        self.vel_sub = rospy.Subscriber(
+            "/cmd_vel",
+            Twist,
+            self.velocity_callback,
+        )
+        self.joy_vel_sub = rospy.Subscriber(
+            "joy_cmd_vel",  # Originally cmd_vel but remapped.
+            Twist,
+            self.joystick_vel_callback,
+        )
+
+        self.collision_lock = Lock()
+
+    def joystick_vel_callback(self, vel: Twist) -> None:
+        """
+        Callback function that receives incoming joystick
+        velocity.
+
+        :param vel:
+            Velocity which stores the linear and angular velocities in the
+            x, y and z directions.
+        """
+        with self.collision_lock:
+            linear_x = vel.linear.x
+            self.run(linear_x)
 
     def velocity_callback(self, vel: Twist) -> None:
         """
@@ -56,13 +81,25 @@ class ReverseSafetySensor:
         :param vel:
             Stores the linear and angular velocities in the x, y and z directions.
         """
+        with self.collision_lock:
+            linear_x = vel.linear.x
+            self.run(linear_x)
+
+    def run(self, velocity: float) -> None:
+        """
+        Determines if there is a possible collision when navigating in reverse.
+
+        :param velocity:
+            Linear velocity in the x.
+        """
         collision_msg = Collision()
-        if vel.linear.x < 0:
-            collision_msg.collisionType = REAR
-            collision_msg.distance = self.collision_sensor.get_distance()
-            collision_msg.min_collision_dist = self.collision_threshold
-            self.rear_collision_pub.publish(collision_msg)
-            rospy.loginfo(f"Distance:[{collision_msg.distance:.6f}] m")
+        with self.collision_lock:
+            if velocity < 0:
+                collision_msg.collisionType = REAR
+                collision_msg.distance = self.collision_sensor.get_distance()
+                collision_msg.min_collision_dist = self.collision_threshold
+                self.rear_collision_pub.publish(collision_msg)
+                rospy.loginfo(f"Distance:[{collision_msg.distance:.6f}] m")
 
 
 if __name__ == "__main__":
