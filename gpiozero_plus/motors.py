e75b8f21573f046997f9059f353f5fc6f936c636
Julian <julianrendon514@gmail.com>
2023-09-15 02:32:45 -0400

    Update motors.py

---
+++ gpiozero_plus/motors.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+A Python class for controlling motors using gpiozero library.
+
+This script defines a class, MotorController, to control motors using the gpiozero library.
+You can set the speed of the individual motors to allow maneuverability in any direction.
+
+Author: Julian A Rendon
+Copyright (c) 2023
+License: MIT License
+Last Updated: September 15, 2023
+
+"""
+from gpiozero import Robot
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+from gpiozero_plus.enums import Motion
+
+
+class MotorController:
+    """
+    Controller for motors attached to the Yahboom G1 Tank Expansion Board.
+
+    Parameters:
+        `left_motor`:
+            Pins associated with the left motor.
+
+        `right_motor`:
+            Pins associated with the right motor.
+
+        `curve_scale`:
+            Amount of curvature when moving in a CURVED motion (default is 0.6).
+    """
+
+    def __init__(self, left_motor: int, right_motor: int, curve_scale: float = 0.6) -> None:
+        self.__factory = PiGPIOFactory()
+        self.__motors = Robot(left=left_motor, right=right_motor, pin_factory=self.__factory)
+        self.__curve_scale = curve_scale
+        self.__motion_type = None
+
+    def run(self, linear_x: float, angular_z: float) -> None:
+        """
+        Adjust motor speed based on the provided velocities.
+
+        Args:
+            `linear_x` (float): Linear velocity in the x direction.
+
+            `angular_z` (float): Angular velocity in the z direction.
+
+        Returns:
+            None
+        """
+        if linear_x > 0.0 and angular_z > 0.0:
+            self.__motors.forward(angular_z, curve_left=self.__curve_scale)
+            self.__motion_type = Motion.LEFT_FWD
+
+        elif linear_x > 0.0 and angular_z < 0.0:
+            self.__motors.forward(angular_z, curve_right=self.__curve_scale)
+            self.__motion_type = Motion.RIGHT_FWD
+
+        elif linear_x < 0.0 and angular_z > 0.0:
+            self.__motors.backward(abs(angular_z), curve_right=self.__curve_scale)
+            self.__motion_type = Motion.RIGHT_BK
+
+        elif linear_x < 0.0 and angular_z < 0.0:
+            self.__motors.backward(abs(angular_z), curve_right=self.__curve_scale)
+            self.__motion_type = Motion.LEFT_BK
+
+        elif linear_x > 0:
+            self.__motors.forward(linear_x)
+            self.__motion_type = Motion.STR_FWD
+
+        elif linear_x < 0:
+            self.__motors.backward(abs(linear_x))
+            self.__motion_type = Motion.STR_BK
+
+        elif angular_z > 0:
+            self.__motors.left(angular_z)
+            self.__motion_type = Motion.ROT_LEFT
+
+        elif angular_z < 0:
+            self.__motors.right(abs(angular_z))
+            self.__motion_type = Motion.ROT_RIGHT
+
+        else:
+            self.__motors.stop()
+            self.__motion_type = Motion.NO_MOTION
+
+    def get_motion_type(self) -> Motion:
+        """
+        Get the current motion type.
+
+        Returns:
+            The current motion type, or None if there's no motion.
+        """
+        return self.__motion_type
+
+    def disconnect(self) -> None:
+        """
+        Stop the motors and release motor pins.
+
+        Returns:
+            None
+        """
+        self.__motors.stop()
+        self.__factory.close()
